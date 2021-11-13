c95d9be6c41e80c7d950539f0907cf1120bd1f96
Aditya Sonani <adityasonani19@gmail.com>
2021-11-13 16:14:47 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shortlink-app';
+  formdata:any;
+  longUrl:any;
+  postURI:any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.formdata = new FormGroup({
+       longUrl: new FormControl("")
+    });
+ }
+
+  onClickSubmit(data:any) {
+    this.longUrl=data.longUrl;
+    this.postURI = 'http://localhost:8080/generate'
+    this.http.post<any>(this.postURI, data).subscribe(
+      (response)=>console.log(response),
+      (err) => console.log(err)
+    );
+  }
 }
