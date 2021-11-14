916e10b329bd14e3d8249e5cf01d2032285625f6
Aditya Sonani <adityasonani19@gmail.com>
2021-11-14 18:06:23 +0530

    Added working frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ export class AppComponent {
   formdata:any;
   longUrl:any;
   postURI:any;
+  shortUrl:any;
 
   constructor(private http: HttpClient) { }
 
@@ -22,11 +24,23 @@ export class AppComponent {
  }
 
   onClickSubmit(data:any) {
-    this.longUrl=data.longUrl;
-    this.postURI = 'http://localhost:8080/generate'
-    this.http.post<any>(this.postURI, data).subscribe(
-      (response)=>console.log(response),
-      (err) => console.log(err)
-    );
+    if (!data || data.longUrl==null) {
+      alert("Please provide some url to shorten!");   
+    }
+    else {
+      this.longUrl=data.longUrl; 
+      if (this.longUrl.length>0) {
+        this.postURI = environment.APIURL+'/generate'
+        // this.postedData = this.http.post<any>(this.postURI, data);
+        this.http.post<any>(this.postURI, data).subscribe(
+          (response)=>this.shortUrl = environment.APIURL+"/"+response.shortUrl,
+          (err) => console.log(err)
+        );
+        this.formdata.reset();
+      } else {
+        alert("Please provide some url to shorten!");   
+      }
+    }
   }
+
 }
